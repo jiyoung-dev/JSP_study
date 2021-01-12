c1aa4bfdeed25ebb0c191eb367db16f7ffa0fc9f
Jiyoung <janice10508@gmail.com>
2021-01-13 02:08:27 +0900

    add file

---
+++ 서블릿 구현하기/LifeCycleServlet.java
@@ -0,0 +1,27 @@
+package lifeWeb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LifeCycleServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		System.out.println("service call");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy call");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init call");
+	}
+	
+}
